c199a09ed517e360cdc07f1f08abbaea9b160adc
Anton <genesiz1990@gmail.com>
2025-05-06 21:34:33 +0300

    init

---
+++ src/main/java/ru/genant/tgptime/TgptimeApplication.java
@@ -0,0 +1,13 @@
+package ru.genant.tgptime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TgptimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TgptimeApplication.class, args);
+	}
+
+}
